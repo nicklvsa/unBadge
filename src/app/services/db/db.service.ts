93426f0fbe118a13235788f6963d14b8d4b0e25e
nicksdesk <nick@charliesdesk.com>
2019-07-27 18:26:08 -0400

    updated comps and started writing db framer

--- src/app/services/db/db.service.ts
+++ src/app/services/db/db.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+// need to import http utils
 
 @Injectable({
   providedIn: 'root'
 })
 export class DBService {
 
-  constructor() { }
+
+  /*
+
+    not using firebase for now...
+    prob php backend
+
+    (possibly convert to firebase in the future)
+
+  */
+
+  constructor(private router: Router) {
+    // init connection to backend
+  }
+
+  // setup authentication endpoints
+
+  googleAuth(): boolean {
+    return false;
+  }
+
+  cleverAuth(): boolean {
+    return false;
+  }
+
+  
+
+  private authCache(item: string) {
+    return (localStorage.getItem(item) != null) ? localStorage.getItem(item) : '';
+  }
+
+
 }
